e8dfc331c547ae4011363805b2fa42f85fdd1ea3
Haider Ali <me@haiderali.dev>
2023-10-06 03:19:39 +0530

    fix undefined global vim

--- .config/nvim/lua/config/langserver.lua
+++ .config/nvim/lua/config/langserver.lua
@@ -2,10 +2,20 @@ local mason = require('mason')
 local masonlspconfig = require('mason-lspconfig')
 local lspconfig = require('lspconfig')
 local coq = require('coq')
+local coq3p = require('coq_3p')
 
 mason.setup()
 masonlspconfig.setup()
 
 lspconfig.pyright.setup(coq.lsp_ensure_capabilities())
 lspconfig.tsserver.setup(coq.lsp_ensure_capabilities())
-lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities())
+lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities({
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            }
+        }
+    }
+}))
+
